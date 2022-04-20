e2bd33610006a461f9c7c246aa7cda4e11b99928
Agarwal Mahaveer <mveer.jaipur@gmail.com>
2022-04-20 11:37:42 +0530

    Setup Navigation between container and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,24 @@
 import { mount } from "marketing/MarketingApp";
+import { useHistory } from "react-router-dom";
 import React, { useRef, useEffect } from "react";
 
 const MarketingApp = () => {
   const ref = useRef(null);
+  const histroy = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = histroy.location;
+
+        if (pathname !== nextPathname) {
+          histroy.push(nextPathname);
+        }
+      },
+    });
+
+    histroy.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
